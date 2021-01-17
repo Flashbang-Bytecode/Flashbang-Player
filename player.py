5f90d82205d09ceab9ef7bdffd8e871e53f98c54
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-17 18:51:57 -0500

    Add debug printing
    This update is compliant with 33% of the Flashbang standards as of the current commit. Really shows how limited Flashbang is right now.

--- player.py
+++ player.py
@@ -1,8 +1,28 @@
 import binascii
 import sys
+# From https://stackoverflow.com/questions/9475241/split-string-every-nth-character
+from itertools import islice
 
+def split_every(n, iterable):
+    i = iter(iterable)
+    piece = list(islice(i, n))
+    while piece:
+        yield piece
+        piece = list(islice(i, n))
 if len(sys.argv) != 1:
     print('You have to drag the file onto the player first...')
 with open(sys.argv[1],'rb') as f:
-    print('Bytecode opened. Now Playing...')
-    print('I said in the Wiki there was no official player. Though someone is working on one. See https://www.youtube.com/watch?v=dQw4w9WgXcQ ;)')
+    print('Bytecode opened. Now Preparing...')
+    hexdata = binascii.hexlify(f.read())
+    print('Hex data created...')
+    print('Creating common variables')
+    fvars = {}
+    print('Done')
+    print('Launching...')
+    splithex = split_every(8, hexdata)
+    # Now we check the first byte to see our instruction
+    if splithex[:2] == b'01':
+        # Debug print, dehexlify the next 3 bytes.
+        print(binascii.dehexlify(splithex[2:8]))
+    # That's all for now, testing time :)
+    
